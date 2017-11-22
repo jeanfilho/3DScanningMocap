6f975ffc75668c85606b2aa9f2f561673dbf12b7
Jean Paul Vieira Filho <jeanfilho@gmail.com>
2017-11-22 11:20:51 +0100

    Added exercise 3

---
+++ Exercise_3/ProcrustesAligner.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "SimpleMesh.h"
+
+class ProcrustesAligner {
+public:
+	Matrix4f estimatePose(const std::vector<Vector3f>& sourcePoints, const std::vector<Vector3f>& targetPoints) {
+		ASSERT(sourcePoints.size() == targetPoints.size() && "The number of source and target points should be the same, since every source point is matched with corresponding target point.");
+
+		// We estimate the pose between source and target points using Procrustes algorithm.
+		// Our shapes have the same scale, therefore we don't estimate scale. We estimated rotation and translation
+		// from source points to target points.
+
+		auto sourceMean = computeMean(sourcePoints);
+		auto targetMean = computeMean(targetPoints);
+		
+		Matrix3f rotation = estimateRotation(sourcePoints, sourceMean, targetPoints, targetMean);
+		Vector3f translation = computeTranslation(sourceMean, targetMean, rotation);
+
+		Matrix4f estimatedPose = Matrix4f::Identity();
+		estimatedPose.block(0, 0, 3, 3) = rotation;
+		estimatedPose.block(0, 3, 3, 1) = translation;
+		return estimatedPose;
+	}
+
+private:
+	Vector3f computeMean(const std::vector<Vector3f>& points) {
+		// TODO: Compute the mean of input points.
+
+		return Vector3f::Zero();
+	}
+
+	Matrix3f estimateRotation(const std::vector<Vector3f>& sourcePoints, const Vector3f& sourceMean, const std::vector<Vector3f>& targetPoints, const Vector3f& targetMean) {
+		// TODO: Estimate the rotation from source to target points, following the Procrustes algorithm. 
+		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
+		
+		return Matrix3f::Identity();
+	}
+
+	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean, const Matrix3f& rotation) {
+		// TODO: Compute the translation vector from source to target opints.
+		
+		return Vector3f::Zero();
+	}
+};
