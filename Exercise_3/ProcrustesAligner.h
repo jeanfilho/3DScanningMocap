d5b1745d71837e770ed905609e0804847dc89153
Jean <jeanfilho@gmail.com>
2017-11-30 20:41:55 +0100

    Task 1 complete

--- Exercise_3/ProcrustesAligner.h
+++ Exercise_3/ProcrustesAligner.h
@@ -27,19 +27,44 @@ private:
 	Vector3f computeMean(const std::vector<Vector3f>& points) {
 		// TODO: Compute the mean of input points.
 
-		return Vector3f::Zero();
+		Vector3f mean(0, 0, 0);
+		for(int i = 0; i < points.size(); i++)
+		{
+			mean += points[i];
+		}
+		mean /= points.size();
+
+		return mean;
 	}
 
 	Matrix3f estimateRotation(const std::vector<Vector3f>& sourcePoints, const Vector3f& sourceMean, const std::vector<Vector3f>& targetPoints, const Vector3f& targetMean) {
 		// TODO: Estimate the rotation from source to target points, following the Procrustes algorithm. 
 		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
-		
-		return Matrix3f::Identity();
+
+		MatrixXf Xs(sourcePoints.size(), 3);
+		for (int i = 0; i < sourcePoints.size(); i++)
+		{
+			Xs(i, 0) = sourcePoints[i].x() - sourceMean.x();
+			Xs(i, 1) = sourcePoints[i].y() - sourceMean.y();
+			Xs(i, 2) = sourcePoints[i].z() - sourceMean.z();
+		}
+
+		MatrixXf Xt(targetPoints.size(), 3);
+		for (int i = 0; i < targetPoints.size(); i++)
+		{
+			Xt(i, 0) = targetPoints[i].x() - targetMean.x();
+			Xt(i, 1) = targetPoints[i].y() - targetMean.y();
+			Xt(i, 2) = targetPoints[i].z() - targetMean.z();
+		}
+
+		JacobiSVD<Matrix3f> svd(Xt.transpose()*Xs, ComputeFullU | ComputeFullV);
+		return  svd.matrixU() * svd.matrixV().transpose();
 	}
 
 	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean, const Matrix3f& rotation) {
 		// TODO: Compute the translation vector from source to target opints.
+		auto translation = targetMean - rotation * sourceMean;
 		
-		return Vector3f::Zero();
+		return translation;
 	}
 };
